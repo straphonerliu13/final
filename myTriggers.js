844275d4b458b66f5139aad79f5775802a50df81
Aainbinder <Andrew.ainbinder@gmail.com>
2021-03-11 15:41:04 -0600

    Chart optimizations

--- myTriggers.js
+++ myTriggers.js
@@ -12,7 +12,56 @@ firebase.auth().onAuthStateChanged(async function(user) {
 
     let response = await fetch( `/.netlify/functions/get_triggers?range=90`)
     let triggers = await response.json()
-    renderTriggers(triggers)
+
+    let chartData = triggers[triggers.length-1]
+    let ctx = document.getElementById('myChart').getContext('2d');
+    let chart = new Chart(ctx, {
+      // The type of chart we want to create
+      type: 'bar',
+  
+      // The data for our dataset
+      data: {
+          labels: ['Anxious', 'Guilty', 'Happy', 'Sad', 'Shame', 'Other'],
+          datasets: [{
+              //label: 'My Trigger History',
+              //backgroundColor: 'rgb(255, 99, 132)',
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.7)',
+                'rgba(54, 162, 235, 0.7)',
+                'rgba(255, 206, 86, 0.7)',
+                'rgba(75, 192, 192, 0.7)',
+                'rgba(153, 102, 255, 0.7)',
+                'rgba(255, 159, 64, 0.7)'
+            ],
+              borderColor: 'rgb(255, 99, 132)',
+              data: []
+          }]
+      },
+  
+      // Configuration options go here
+      options: {
+        responsive: false,
+        legend: {
+          display: false
+        },
+        title: {
+          display: true,
+          text: 'My Trigger History',
+          fontSize: 28,
+          position: 'bottom'
+        },
+        scales: {
+          yAxes: [{
+              ticks: {
+                  display: true,
+                  stepSize: 1
+              }
+          }]
+        } 
+      }
+    });
+
+    renderTriggers(triggers, chart)
 
   document.querySelector('.sign-in-or-sign-out').insertAdjacentHTML('beforebegin',`
     <h1 class="w-1/6 text-m text-black">Hello, ${user.displayName}!</h1>
@@ -33,7 +82,7 @@ firebase.auth().onAuthStateChanged(async function(user) {
     document.querySelector('.myTriggers').innerHTML = ""
     let response = await fetch( `/.netlify/functions/get_triggers?range=7`)
     let triggers = await response.json()
-    renderTriggers(triggers)
+    renderTriggers(triggers, chart)
   })
 
   document.querySelector('.rangeThirtyDayTriggers').addEventListener('click', async function(event){
@@ -41,7 +90,7 @@ firebase.auth().onAuthStateChanged(async function(user) {
     document.querySelector('.myTriggers').innerHTML = ""
     let response = await fetch( `/.netlify/functions/get_triggers?range=30`)
     let triggers = await response.json()
-    renderTriggers(triggers)
+    renderTriggers(triggers, chart)
   })
 
   document.querySelector('.rangeNinetyDayTriggers').addEventListener('click', async function(event){
@@ -49,7 +98,7 @@ firebase.auth().onAuthStateChanged(async function(user) {
     document.querySelector('.myTriggers').innerHTML = ""
     let response = await fetch( `/.netlify/functions/get_triggers?range=90`)
     let triggers = await response.json()
-    renderTriggers(triggers)
+    renderTriggers(triggers, chart)
   })
 
 
@@ -72,7 +121,7 @@ firebase.auth().onAuthStateChanged(async function(user) {
     ui.start('.sign-in-or-sign-out', authUIConfig)
   }
 
-  function renderTriggers(triggers){
+  function renderTriggers(triggers, chart){
     for(let i = 0; i < triggers.length; i++){
       let trigger = triggers[i]
       //console.log(trigger.triggerDate)
@@ -81,55 +130,11 @@ firebase.auth().onAuthStateChanged(async function(user) {
           <span class="date">${trigger.month}/${trigger.date}/${trigger.year} | </span>
           <span class="trigger">${trigger.emotion}</span>
         </div>
-        
       `)
     }
-      let chartData = triggers[triggers.length-1]
-      let ctx = document.getElementById('myChart').getContext('2d');
-      let chart = new Chart(ctx, {
-        // The type of chart we want to create
-        type: 'bar',
-    
-        // The data for our dataset
-        data: {
-            labels: ['Anxious', 'Guilty', 'Happy', 'Sad', 'Shame', 'Other'],
-            datasets: [{
-                //label: 'My Trigger History',
-                //backgroundColor: 'rgb(255, 99, 132)',
-                backgroundColor: [
-                  'rgba(255, 99, 132, 0.7)',
-                  'rgba(54, 162, 235, 0.7)',
-                  'rgba(255, 206, 86, 0.7)',
-                  'rgba(75, 192, 192, 0.7)',
-                  'rgba(153, 102, 255, 0.7)',
-                  'rgba(255, 159, 64, 0.7)'
-              ],
-                borderColor: 'rgb(255, 99, 132)',
-                data: [chartData.anxCount, chartData.guiltCount, chartData.happyCount, chartData.sadCount, chartData.shameCount, chartData.otherCount]
-            }]
-        },
-    
-        // Configuration options go here
-        options: {
-          responsive: false,
-          legend: {
-            display: false
-          },
-          title: {
-            display: true,
-            text: 'My Trigger History',
-            fontSize: 28,
-            position: 'bottom'
-          },
-          scales: {
-            yAxes: [{
-                ticks: {
-                    display: true,
-                    stepSize: 1
-                }
-            }]
-        }
-        }
-    });
+
+    let chartData = triggers[triggers.length-1]
+    chart.data.datasets[0].data = [chartData.anxCount, chartData.guiltCount, chartData.happyCount, chartData.sadCount, chartData.shameCount, chartData.otherCount]
+    chart.update()
   }
 })
