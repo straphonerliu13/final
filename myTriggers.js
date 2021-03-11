853f44aafa7091fd23dcf12dc127260732833554
Aainbinder <Andrew.ainbinder@gmail.com>
2021-03-11 14:32:19 -0600

    Chart sizing fixed

--- myTriggers.js
+++ myTriggers.js
@@ -12,7 +12,6 @@ firebase.auth().onAuthStateChanged(async function(user) {
 
     let response = await fetch( `/.netlify/functions/get_triggers?range=90`)
     let triggers = await response.json()
-    let chartData = triggers[triggers.length-1]
     renderTriggers(triggers)
 
   document.querySelector('.sign-in-or-sign-out').insertAdjacentHTML('beforebegin',`
@@ -53,25 +52,6 @@ firebase.auth().onAuthStateChanged(async function(user) {
     renderTriggers(triggers)
   })
 
-  var ctx = document.getElementById('myChart').getContext('2d');
-  var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'bar',
-
-    // The data for our dataset
-    data: {
-        labels: ['Anxious', 'Guilty', 'Happy', 'Sad', 'Shame', 'Other'],
-        datasets: [{
-            label: 'My Trigger History',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [chartData.anxCount, chartData.guiltCount, chartData.happyCount, chartData.sadCount, chartData.shameCount, chartData.otherCount]
-        }]
-    },
-
-    // Configuration options go here
-    options: {}
-});
 
   } else {
     // Signed out
@@ -104,5 +84,27 @@ firebase.auth().onAuthStateChanged(async function(user) {
         
       `)
     }
+      let chartData = triggers[triggers.length-1]
+      let ctx = document.getElementById('myChart').getContext('2d');
+      let chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'bar',
+    
+        // The data for our dataset
+        data: {
+            labels: ['Anxious', 'Guilty', 'Happy', 'Sad', 'Shame', 'Other'],
+            datasets: [{
+                label: 'My Trigger History',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [chartData.anxCount, chartData.guiltCount, chartData.happyCount, chartData.sadCount, chartData.shameCount, chartData.otherCount]
+            }]
+        },
+    
+        // Configuration options go here
+        options: {
+          responsive: false 
+        }
+    });
   }
 })
