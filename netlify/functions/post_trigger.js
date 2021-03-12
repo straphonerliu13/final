97d220e20ee2113a5416210d57b46a715210681e
Linda Pan <linda.pan@kellogg.northwestern.edu>
2021-03-11 19:11:21 -0600

    creating lambda function

---
+++ netlify/functions/post_trigger.js
@@ -0,0 +1,29 @@
+let firebase = require('./firebase')
+
+exports.handler = async function(event) {
+
+    let db = firebase.firestore()
+    console.log(event)
+    let body = JSON.parse(event.body)
+    let userId = ''
+    let created = ''
+    let triggerDate = ''
+    let triggerDetail = ''
+    let triggerAngry = ''
+    let triggerAnxious = ''
+    let triggerGuilt = ''
+    let triggerSad = ''
+    let triggerShame = ''
+    let triggerOther = ''
+
+
+
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            thingToKnow: 'This is working'
+        }
+      }
+}
+
